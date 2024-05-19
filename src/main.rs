8854b6c147076c784c5c92cda068c0f1d64aedcc
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-05-19 14:57:45 +0530

    qol

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ mod matrix;
 // Example usage
 fn main() {
     // Create a new matrix
-    let mut matrix = Matrix::from_file("matrix.txt").unwrap();
+    let mut matrix1 = Matrix::from_file("matrix.txt").unwrap();
 
     // Zoom the matrix
-    matrix.zoom(3.0);
+    matrix1.zoom(2.0);
 
-    println!("{}", matrix);
+    println!("{}", matrix1);
+
 }
